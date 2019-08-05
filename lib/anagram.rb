1e28abd9ae01c9b0d42bfad54e6a6a931a304bd1
Will Hodgson <will.hodgson@current-rms.com>
2019-08-05 10:59:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,8 +9,8 @@ class Anagram
   def match(word_list)
     anagrams = []
     word_list.each do |word|
-      split_word = word.split().sort {|a, b| a <=> b}
-      split_instance_word = @word.split().sort {|a, b| a <=> b}
+      split_word = word.split(', ').sort {|a, b| a <=> b}
+      split_instance_word = @word.split(', ').sort {|a, b| a <=> b}
       if split_word == split_instance_word
         anagrams << word
       end
